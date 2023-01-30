aef18dd7b15cfff94663048210256a1fd4fc9991
DharshithHari24 <96918607+DharshithHari24@users.noreply.github.com>
2023-01-30 19:54:08 +0530

    Create tryitout.py

---
+++ tryitout.py
@@ -0,0 +1,20 @@
+# try printing text indirectly 
+
+
+# print text directly
+
+
+#create input box for password
+
+
+# -- find the following by the codee
+fruits = ["apple","banana","kiwi","mango","lemon","grapes"]
+
+# Print the list
+
+
+# Find the index value of grapes
+
+
+# Find the length of this list
+
